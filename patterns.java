e3a1252b52b9e3d3d50675a649a0b5b4f58e2ac9
sv742744 <shashi09op@gmail.com>
2024-12-07 11:45:18 +0530

    Add files via upload

---
+++ patterns.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class patterns {
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if((n%2)==0)
+            n-=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==n/2 || i==0 || i==n-1 )
+                    System.out.print("*"+" ");//for printting numbers use only j//
+                else if((i<n/2&&j==0)||(j==n-1&&i>n/2))
+                    System.out.print("*"+" ");
+                else
+                     System.out.print(" "+" ");
+            }
+            System.out.println();
+        }
+    }
+}
