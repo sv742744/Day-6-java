c8805cdfabeebcc1c15fa4902f18d5c077ae240c
sv742744 <shashi09op@gmail.com>
2024-12-07 12:42:45 +0530

    Add files via upload

---
+++ hallowtraingle2.java
@@ -0,0 +1,23 @@
+/*
+        * 
+      * *
+    *   *
+  *     *
+* * * * *
+*/
+import java.util.*;
+class hallowtraingle2 {
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j==n-1 || i==n-1 || i+j==n-1)
+                    System.out.print("*"+" ");//for printting numbers use only j//
+                else
+                     System.out.print(" "+" ");
+            }
+            System.out.println();
+        }
+    }
+}
